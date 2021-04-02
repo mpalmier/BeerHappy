119cc348068adeb9d55536025e5514c4f37717fa
Max170602 <besserve.maxence@gmail.com>
2021-04-02 15:29:04 +0200

    ProduitDAO -> 2.0

--- DAO/ProduitDAO.php
+++ DAO/ProduitDAO.php
@@ -2,29 +2,75 @@
 
 
 
-class ProduitDAO {
+class ProduitDAO
+{
 
-    public static function getProduit() {
+    public static function getProduit()
+    {
         $bdd = DatabaseLinker::getConnexion();
         $reponse = $bdd->prepare("SELECT * from produit");
         $reponse->execute();
         $produit = $reponse->fetchAll();
-        $tab=Array();
-        if (empty($produit[0])){
+        $tab = array();
+        if (empty($produit[0])) {
             return null;
-        }
-        else{
+        } else {
             foreach ($produit as $produits) {
-                $produitDTO = new UserDTO();
+                $produitDTO = new ProduitDTO();
                 $produitDTO->setIdProduit($produits[0]);
                 $produitDTO->setNom($produits[1]);
                 $produitDTO->setPrix($produits[2]);
                 $produitDTO->setCategorie($produits[3]);
                 $produitDTO->setPhoto($produits[4]);
-                $tab[]=$produitDTO;
+                $tab[] = $produitDTO;
             }
-        
+
             return $tab;
         }
     }
+
+    public static function getProduitByCategorie($categorie)
+    {
+        $bdd = DatabaseLinker::getConnexion();
+        $reponse = $bdd->prepare("SELECT * from produit where categorie=?");
+        $reponse->execute(array($categorie));
+        $produit = $reponse->fetchAll();
+        $tab = array();
+        if (empty($produit[0])){
+            return null;
+        }
+        else{
+            $lproduit=$produit[0];
+            $produitDTO = new ProduitDTO();
+            $produitDTO->setIdProduit($lproduit[0]);
+            $produitDTO->setNom($lproduit[1]);
+            $produitDTO->setPrix($lproduit[2]);
+            $produitDTO->setCategorie($lproduit[3]);
+            $produitDTO->setPhoto($lproduit[4]);
+            $tab[] = $produitDTO;
+            return $tab;
+        }
+    }
+
+    public static function getProduitById($id)
+    {
+        $bdd = DatabaseLinker::getConnexion();
+        $reponse = $bdd->prepare("SELECT * from produit where id=?");
+        $reponse->execute(array($id));
+        $produit = $reponse->fetchAll();
+        if (empty($produit[0])){
+            return null;
+        }
+        else{
+            $lproduit=$produit[0];
+            $produitDTO = new ProduitDTO();
+            $produitDTO->setIdProduit($lproduit[0]);
+            $produitDTO->setNom($lproduit[1]);
+            $produitDTO->setPrix($lproduit[2]);
+            $produitDTO->setCategorie($lproduit[3]);
+            $produitDTO->setPhoto($lproduit[4]);
+            return $produitDTO;
+        }
+    }
+
 }
