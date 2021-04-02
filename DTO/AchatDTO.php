11e8cb0301193d17e1aa18a8d0aa18ecd915608a
Mathéo <mat42130@hotmail.fr>
2021-04-02 13:40:48 +0200

    dto

--- DTO/AchatDTO.php
+++ DTO/AchatDTO.php
@@ -1,5 +1,14 @@
 <?php
 
 class AchatDTO{
-    
+    private $id;
+    function getId() {
+        return $this->id;
+    }
+
+    function setId($id): void {
+        $this->id = $id;
+    }
+
+
 }
