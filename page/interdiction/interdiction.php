73a0fbad925606fbf1439bff7a2b9dc7faef1933
Mathéo <mat42130@hotmail.fr>
2021-04-02 15:47:26 +0200

    page interdiction

--- page/interdiction/interdiction.php
+++ page/interdiction/interdiction.php
@@ -1,5 +1,4 @@
 <?php
 ?>
-<div class="preference">
-    <input type="button" name="cheese" id="cheese">
-</div>
+<p> Avez vous plus de 18 ans ? </p>
+<a href="index.php?page=acceuil"><p> Oui je suis conscient des risques.</p> </a>
