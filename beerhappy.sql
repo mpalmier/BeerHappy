e356cdbdd77c5f557ab16165185688cc7cc1dc62
Mathéo <mat42130@hotmail.fr>
2021-04-02 13:25:53 +0200

    la bdd en bon état

---
+++ beerhappy.sql
@@ -0,0 +1,163 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : ven. 02 avr. 2021 à 11:25
+-- Version du serveur :  5.7.31
+-- Version de PHP : 7.3.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `beerhappy`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `achat`
+--
+
+DROP TABLE IF EXISTS `achat`;
+CREATE TABLE IF NOT EXISTS `achat` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user_achat` int(11) DEFAULT NULL,
+  `id_achat` int(11) DEFAULT NULL,
+  `id_user` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `achat_user0_FK` (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `appartient`
+--
+
+DROP TABLE IF EXISTS `appartient`;
+CREATE TABLE IF NOT EXISTS `appartient` (
+  `id_panier` int(11) NOT NULL,
+  `id_produit` int(11) NOT NULL,
+  PRIMARY KEY (`id_panier`,`id_produit`),
+  KEY `appartient_produit1_FK` (`id_produit`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `message`
+--
+
+DROP TABLE IF EXISTS `message`;
+CREATE TABLE IF NOT EXISTS `message` (
+  `id_message` int(11) NOT NULL AUTO_INCREMENT,
+  `pseudo_user` varchar(30) DEFAULT NULL,
+  `content` varchar(250) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `id_user` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_message`),
+  KEY `message_user0_FK` (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `panier`
+--
+
+DROP TABLE IF EXISTS `panier`;
+CREATE TABLE IF NOT EXISTS `panier` (
+  `id_panier` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user_panier` int(11) DEFAULT NULL,
+  `id_produit` int(11) DEFAULT NULL,
+  `id_user` int(11) DEFAULT NULL,
+  `id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_panier`),
+  UNIQUE KEY `panier_user0_AK` (`id_user`),
+  KEY `panier_achat1_FK` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `produit`
+--
+
+DROP TABLE IF EXISTS `produit`;
+CREATE TABLE IF NOT EXISTS `produit` (
+  `id_produit` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(30) DEFAULT NULL,
+  `prix` float DEFAULT NULL,
+  `categorie` varchar(30) DEFAULT NULL,
+  `photo` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id_produit`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id_user` int(11) NOT NULL AUTO_INCREMENT,
+  `pseudo` varchar(30) DEFAULT NULL,
+  `mdp` varchar(30) DEFAULT NULL,
+  `argent` varchar(30) DEFAULT NULL,
+  `adresse` varchar(30) DEFAULT NULL,
+  `admin` int(11) DEFAULT NULL,
+  `id_panier` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_user`),
+  UNIQUE KEY `user_panier0_AK` (`id_panier`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `achat`
+--
+ALTER TABLE `achat`
+  ADD CONSTRAINT `achat_user0_FK` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+
+--
+-- Contraintes pour la table `appartient`
+--
+ALTER TABLE `appartient`
+  ADD CONSTRAINT `appartient_panier0_FK` FOREIGN KEY (`id_panier`) REFERENCES `panier` (`id_panier`),
+  ADD CONSTRAINT `appartient_produit1_FK` FOREIGN KEY (`id_produit`) REFERENCES `produit` (`id_produit`);
+
+--
+-- Contraintes pour la table `message`
+--
+ALTER TABLE `message`
+  ADD CONSTRAINT `message_user0_FK` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+
+--
+-- Contraintes pour la table `panier`
+--
+ALTER TABLE `panier`
+  ADD CONSTRAINT `panier_achat1_FK` FOREIGN KEY (`id`) REFERENCES `achat` (`id`),
+  ADD CONSTRAINT `panier_user0_FK` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+
+--
+-- Contraintes pour la table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_panier0_FK` FOREIGN KEY (`id_panier`) REFERENCES `panier` (`id_panier`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
