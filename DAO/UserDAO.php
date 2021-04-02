2cc075e3473b8d40b97476e204771d14030412e1
Mathéo <mat42130@hotmail.fr>
2021-04-02 13:49:46 +0200

    Update UserDAO.php

--- DAO/UserDAO.php
+++ DAO/UserDAO.php
@@ -1,5 +1,53 @@
 <?php
 
 class UserDAO{
+    public static function getUser() {
+        $bdd = DatabaseLinker::getConnexion();
+	$reponse = $bdd->prepare("SELECT * from user");
+	$reponse->execute();
+	$user = $reponse->fetchAll();
+	$tab=Array(); 
+	if (empty($user[0])){
+		return null;
+	}
+
+	else{
+		foreach ($user as $users) {
+
+			$userDTO = new UserDTO();
+			$userDTO->setIdUser($users[0]);
+                        $userDTO->setPseudo($users[1]);
+                        $userDTO->setMdp($users[2]);
+                        $userDTO->setArgent($users[3]);
+                        $userDTO->setAdresse($users[4]);
+			$tab[]=$userDTO; 
+		}
+            return $tab;
+        
+        }
+    }
+    public static function getUserById($id,$mdp){
+        $bdd = DatabaseLinker::getConnexion();
+	$reponse = $bdd->prepare("SELECT * from user where pseudo=? and mdp=?");
+	$reponse->execute(array($id,$mdp));
+	$user = $reponse->fetchAll();
+        if (empty($user[0])){
+		return null;
+	}
+
+	else{
+			$userDTO = new UserDTO();
+			$userDTO->setIdUser($users[0]);
+                        $userDTO->setPseudo($users[1]);
+                        $userDTO->setMdp($users[2]);
+                        $userDTO->setArgent($users[3]);
+                        $userDTO->setAdresse($users[4]);
+			return $userDTO;
+                        
+                       
+                }
+                 
+              
+	}
     
 }
