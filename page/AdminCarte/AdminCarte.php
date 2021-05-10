78460dc38195f5a754851d03d33b66cede9a3724
Max170602 <besserve.maxence@gmail.com>
2021-05-10 15:01:16 +0200

    Patch admin Affichage

--- page/AdminCarte/AdminCarte.php
+++ page/AdminCarte/AdminCarte.php
@@ -1,7 +1,22 @@
-<?php
-?>
-<h1> Ajouter une catégorie </h1>
-<form action=index.php?page=AddCategorie method='post'> <input type=text name=nom> <input value='Ajouter' type='submit' > </form><br>
-<?php
-AdminCarteController::afficherCategorie();
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Fixed Header with html and css | Collapsing Header Tutorial</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="assets/css/style_css_commun.css">
+</head>
+<body>
+<div class="content-area">
+    <div class="wrapper">
+
+        <h1> Ajouter une catégorie </h1>
+        <form action=index.php?page=AddCategorie method='post'> <input type=text name=nom> <input value='Ajouter' type='submit' > </form><br>
+        <?php
+        AdminCarteController::afficherCategorie();
+        ?>
+    </div>
+</div>
+</body>
+</html>
 
--- page/AdminProduit/AdminProduit.php
+++ page/AdminProduit/AdminProduit.php
@@ -1,20 +1,36 @@
-<form action="index.php?page=AjouterProduit" method="post" enctype="multipart/form-data">
-    <p> Image du produit :</p>
-    <input type="file" name="upload_file">
-    <p>Nom du produit : <input type="text" name="nom" /></p>
-    <p>Prix du produit : <input type="text" name="prix" /></p>
-    <p>Stock : <input type="text" name="stock" /></p>
-    <p> Catégorie :</p>
-    <select name="menu_destination">
-        <?php
-        ControllerAdminProduit::afficherCategorie();
-        ?>
-    </select>
-    <p><input type="submit" name="submit" value="Ajouter un produit"></p>
-</form>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Fixed Header with html and css | Collapsing Header Tutorial</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="assets/css/style_css_commun.css">
+</head>
+<body>
+<div class="content-area">
+    <div class="wrapper">
+        <form action="index.php?page=AjouterProduit" method="post" enctype="multipart/form-data">
+            <p> Image du produit :</p>
+            <input type="file" name="upload_file">
+            <p>Nom du produit : <input type="text" name="nom" /></p>
+            <p>Prix du produit : <input type="text" name="prix" /></p>
+            <p>Stock : <input type="text" name="stock" /></p>
+            <p> Catégorie :</p>
+            <select name="menu_destination">
+                <?php
+                ControllerAdminProduit::afficherCategorie();
+                ?>
+            </select>
+            <p><input type="submit" name="submit" value="Ajouter un produit"></p>
+        </form>
 
-<?php
-ControllerAdminProduit::afficherListeProduits();
+        <?php
+        ControllerAdminProduit::afficherListeProduits();
+        ?>
+    </div>
+</div>
+</body>
+</html>
 
 
 
